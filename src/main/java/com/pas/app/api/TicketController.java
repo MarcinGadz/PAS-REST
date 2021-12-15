a6ba098eee0801a99d13970e4841510477b6526e
d-wasilewski <230032@edu.p.lodz.pl>
2021-12-15 16:10:55 +0100

    Change User and Ticket controller to rest framework

--- src/main/java/com/pas/app/api/TicketController.java
+++ src/main/java/com/pas/app/api/TicketController.java
@@ -1,87 +1,80 @@
-//package com.pas.app.api;
-//
-//import com.pas.app.managers.TicketManager;
-//import com.pas.app.model.Ticket;
-//
-//import javax.inject.Inject;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.Response;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Path("/ticket")
-//public class TicketController {
-//    private TicketManager manager;
-//
-//    public TicketManager getManager() {
-//        return manager;
-//    }
-//
-//    @Inject
-//    public void setManager(TicketManager manager) {
-//        this.manager = manager;
-//    }
-//
-//    @GET
-//    @Produces("application/json")
-//    public List<Ticket> getAll() {
-//        return manager.getAll();
-//    }
-//
-//    @GET
-//    @Path("/{id}")
-//    @Produces("application/json")
-//    public Response get(@PathParam("id") UUID id) {
-//        Ticket t = manager.getById(id);
-//        if(t == null) {
-//            return Response.status(Response.Status.NOT_FOUND).entity("Not found").build();
-//        }
-//        return Response.ok().entity(t).build();
-//    }
-//
-//    @POST
-//    @Produces("application/json")
-//    @Consumes("application/json")
-//    public Response create(Ticket f) {
-//        if(f.getFilm() == null || f.getSeat() == null || f.getClient() == null
-//                || f.getFilm().getId() == null || f.getSeat().getId() == null
-//        || f.getClient().getId() == null) {
-//            return Response.status(Response.Status.BAD_REQUEST).entity("Wrong parameters").build();
-//        }
-//        try {
-//            f = manager.add(f);
-//        } catch (Exception ex) {
-//            return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
-//        }
-//        return Response.status(Response.Status.CREATED).entity(f).build();
-//    }
-//
-//    @PUT
-//    @Path("/{id}")
-//    @Produces("application/json")
-//    @Consumes("application/json")
-//    public Response update(@PathParam("id") UUID id, Ticket f) {
-//        if(!manager.existsById(id)) {
-//            return Response.status(Response.Status.NOT_FOUND).entity("Not found").build();
-//        }
-//        if(f == null || f.getClient().getId() == null || f.getFilm().getId() == null || f.getSeat() == null) {
-//            return Response.status(Response.Status.BAD_REQUEST).entity("Wrong parameters").build();
-//        }
-//        f = manager.update(id, f);
-//        return Response.ok().entity(f).build();
-//    }
-//
-//    @DELETE
-//    @Path("/{id}")
-//    public Response delete(@PathParam("id") UUID id) {
-//        if(!manager.existsById(id)) {
-//            return Response.status(Response.Status.NOT_FOUND).entity("Not found").build();
-//        }
-//        try {
-//            manager.remove(manager.getById(id));
-//        } catch (Exception ex) {
-//            return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
-//        }
-//        return Response.ok().build();
-//    }
-//}
+package com.pas.app.api;
+
+import com.pas.app.managers.TicketManager;
+import com.pas.app.model.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/ticket")
+public class TicketController {
+    private TicketManager manager;
+
+
+    @Autowired
+    public void TicketController(TicketManager manager) {
+            this.manager = manager;
+        }
+
+    @GetMapping
+    public List<Ticket> getAll() {
+        return manager.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getId(@PathVariable("id") UUID id) {
+        if(!manager.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        Ticket f = manager.getById(id);
+        return ResponseEntity.ok(f);
+    }
+
+    @PostMapping
+    public ResponseEntity<Ticket> create(@RequestBody Ticket f) {
+        if(f.getFilm() == null || f.getSeat() == null || f.getClient() == null
+                || f.getFilm().getId() == null || f.getSeat().getId() == null
+        || f.getClient().getId() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        f = manager.add(f);
+        return new ResponseEntity<>(f, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ticket>  update(@PathVariable("id") UUID id, Ticket f) {
+        if(!manager.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        if(f == null || f.getClient().getId() == null || f.getFilm().getId() == null || f.getSeat() == null) {
+
+            return ResponseEntity.badRequest().build();
+        }
+        f = manager.update(id, f);
+        return new ResponseEntity<>(f, HttpStatus.ACCEPTED);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Ticket> deleteFilm(@PathVariable("id") UUID id) {
+        if(!manager.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        try {
+            manager.remove(manager.getById(id));
+        } catch (Exception ex) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.accepted().build();
+    }
+
+}
--- src/main/java/com/pas/app/api/UserController.java
+++ src/main/java/com/pas/app/api/UserController.java
@@ -1,111 +1,121 @@
-//package com.pas.app.api;
-//
-//import com.pas.app.managers.UserManager;
-//import com.pas.app.model.Ticket;
-//import com.pas.app.model.User;
-//
-//import javax.inject.Inject;
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.Response;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Path("/user")
-//public class UserController {
-//
-//    @Inject
-//    private UserManager manager;
-//
-//    @GET
-//    @Produces("application/json")
-//    public List<User> getAll() {
-//        return manager.getAll();
-//    }
-//
-//    @GET
-//    @Path("/{id}")
-//    @Produces("application/json")
-//    public Response get(@PathParam("id") UUID id) {
-//        if (manager.existsById(id)) {
-//            User user = manager.getById(id);
-//            return Response.status(Response.Status.OK).entity(user).build();
-//        }
-//        return Response.status(Response.Status.NOT_FOUND).build();
-//    }
-//
-//    @GET
-//    @Path("/find")
-//    @Produces("application/json")
-//    public Response findByLogin(@QueryParam("login") String login) {
-//        if (login == null) {
-//            return Response.status(Response.Status.BAD_REQUEST).build();
-//        }
-//        User user = manager.getUser(login);
-//        if(user == null) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//        return Response.status(Response.Status.OK).entity(user).build();
-//    }
-//
-//    @GET
-//    @Path("/findcontaining")
-//    @Produces("application/json")
-//    public Response findWithCharsInLogin(@QueryParam("login") String login) {
-//        if (login == null) {
-//            return null;
-//        }
-//        List<User> users = manager.getWithCharsInLogin(login);
-//        if(users == null || users.isEmpty()) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//        return Response.ok().entity(users).build();
-//    }
-//
-//    @POST
-//    @Path("/{id}/activate")
-//    public Response activate(@PathParam("id") UUID id) {
-//        if (manager.existsById(id)) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//        manager.activate(id);
-//        return Response.ok().build();
-//    }
-//
-//    @POST
-//    @Path("/{id}/deactivate")
-//    public Response deactivate(@PathParam("id") UUID id) {
-//        if (manager.existsById(id)) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//        manager.deactivate(id);
-//        return Response.ok().build();
-//    }
-//
-//    @POST
-//    @Produces("application/json")
-//    @Consumes("application/json")
-//    public Response create(User u) {
-//        if (u == null || u.getFirstName() == null || u.getLastName() == null || u.getLogin() == null) {
-//            return Response.status(Response.Status.BAD_REQUEST).build();
-//        }
-//        u = manager.register(u);
-//        return Response.status(Response.Status.CREATED).entity(u).build();
-//    }
-//
-//    @PUT
-//    @Path("/{id}")
-//    @Produces("application/json")
-//    @Consumes("application/json")
-//    public Response update(@PathParam("id") UUID id, User u) {
-//        if (id == null || u == null || u.getFirstName() == null || u.getLastName() == null || u.getLogin() == null) {
-//            return Response.status(Response.Status.BAD_REQUEST).build();
-//        }
-//        if (!manager.existsById(id)) {
-//            return Response.status(Response.Status.NOT_FOUND).build();
-//        }
-//        User tmp = manager.update(id, u);
-//        return Response.status(Response.Status.OK).entity(tmp).build();
-//    }
+package com.pas.app.api;
+
+import com.pas.app.managers.UserManager;
+import com.pas.app.model.Ticket;
+import com.pas.app.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.pas.app.managers.TicketManager;
+import com.pas.app.model.Ticket;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+
+    private UserManager manager;
+
+    @Autowired
+    public UserController(UserManager manager) {
+        this.manager = manager;
+    }
+
+
+    @GetMapping
+    public List<User> getAll() {
+        return manager.getAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> get(@PathVariable("id") UUID id) {
+        if(!manager.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        User f = manager.getById(id);
+        return ResponseEntity.ok(f);
+    }
+
+    @GetMapping("/find")
+    public ResponseEntity findByLogin(@PathVariable("login") String login) {
+        if(login == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        User user = manager.getUser(login);
+        if(user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/findcontaining/{login}")
+    public ResponseEntity<List<User>> findWithCharsInLogin(@PathVariable("login") String login) {
+                if (login == null) {
+            return ResponseEntity.notFound().build();
+        }
+        List<User> users = manager.getWithCharsInLogin(login);
+        if(users == null || users.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @PutMapping("/{id}/activate")
+    public ResponseEntity activate(@PathVariable("id") UUID id) {
+        if(!manager.existsById(id)) {
+
+            return ResponseEntity.notFound().build();
+        }
+        manager.activate(id);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PutMapping("/{id}/deactivate")
+    public ResponseEntity deactivate(@PathVariable("id") UUID id) {
+        if(!manager.existsById(id)) {
+
+            return ResponseEntity.notFound().build();
+        }
+        manager.deactivate(id);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User u) {
+        if (u == null || u.getFirstName() == null || u.getLastName() == null || u.getLogin() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        u = manager.register(u);
+
+        return new ResponseEntity<>(u, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable("id") UUID id, User u) {
+        if(!manager.existsById(id)) {
+
+            return ResponseEntity.notFound().build();
+        }
+        if (id ==  null || u == null || u.getFirstName() == null || u.getLastName() == null || u.getLogin() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        u = manager.update(id, u);
+
+        return new ResponseEntity<>(u, HttpStatus.ACCEPTED);
+
+    }
+
+
 //
 //    @DELETE
 //    @Path("/{id}")
@@ -117,25 +127,21 @@
 //        return Response.status(Response.Status.NOT_FOUND).build();
 //    }
 //
-//    @GET
-//    @Path("/{id}/active")
-//    @Produces("application/json")
-//    public Response getActive(@PathParam("id") UUID id) {
-//        if (manager.existsById(id)) {
-//            List<Ticket> t = manager.getActiveTickets(id);
-//            return Response.ok().entity(t).build();
-//        }
-//        return Response.status(Response.Status.NOT_FOUND).build();
-//    }
-//
-//    @GET
-//    @Path("/{id}/past")
-//    @Produces("application/json")
-//    public Response getPast(@PathParam("id") UUID id) {
-//        if (manager.existsById(id)) {
-//            List<Ticket> t = manager.getInactiveTickets(id);
-//            return Response.ok().entity(t).build();
-//        }
-//        return Response.status(Response.Status.NOT_FOUND).build();
-//    }
-//}
+
+    @GetMapping("/{id}/active")
+    public ResponseEntity<List<Ticket>> getActive(@PathVariable("id") UUID id) {
+        if(manager.existsById(id)) {
+            List<Ticket> t = manager.getActiveTickets(id);
+            return ResponseEntity.ok(t);
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/{id}/past")
+    public ResponseEntity<List<Ticket>> getPast(@PathVariable("id") UUID id) {
+        if(manager.existsById(id)) {
+            List<Ticket> t = manager.getInactiveTickets(id);
+            return ResponseEntity.ok(t);
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
