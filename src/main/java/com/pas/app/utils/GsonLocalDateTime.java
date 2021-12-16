a1350fd5a0669261d90e207bf9bc354afa002ced
mtosik <michal.tosik@wp.pl>
2021-12-16 11:33:57 +0100

    testy

--- src/main/java/com/pas/app/utils/GsonLocalDateTime.java
+++ src/main/java/com/pas/app/utils/GsonLocalDateTime.java
@@ -1,2 +1,17 @@
-package com.pas.app.utils;public class GsonLocalDateTime {
+package com.pas.app.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.LocalDateTime;
+
+public class GsonLocalDateTime {
+    public static Gson getGsonSerializer(){
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
+                .create();
+    }
+
 }
--- src/main/java/com/pas/app/utils/LocalDateTimeDeserializer.java
+++ src/main/java/com/pas/app/utils/LocalDateTimeDeserializer.java
@@ -1,4 +1,14 @@
 package com.pas.app.utils;
 
-public class LocalDateTimeDeserializer {
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
+import java.time.LocalDateTime;
+
+public class LocalDateTimeDeserializer implements JsonDeserializer<LocalDateTime> {
+    @Override
+    public LocalDateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
+            throws JsonParseException {
+        return LocalDateTime.parse(jsonElement.getAsString());
+    }
 }
