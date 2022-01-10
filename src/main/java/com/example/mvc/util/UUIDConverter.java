c81a4f5753ee4841dec98ac24111c90ea0562352
d-wasilewski <230032@edu.p.lodz.pl>
2022-01-10 21:47:42 +0100

    Validate inputs for all pages

--- src/main/java/com/example/mvc/util/UUIDConverter.java
+++ src/main/java/com/example/mvc/util/UUIDConverter.java
@@ -10,11 +10,17 @@ import java.util.UUID;
 public class UUIDConverter implements Converter<UUID> {
     @Override
     public UUID getAsObject(FacesContext facesContext, UIComponent uiComponent, String s) {
+        if (s == null || s.trim().isEmpty()) {
+            return null;
+        }
         return UUID.fromString(s);
     }
 
     @Override
     public String getAsString(FacesContext facesContext, UIComponent uiComponent, UUID uuid) {
+        if (uuid == null)  {
+            return null;
+        }
         return uuid.toString();
     }
 }
