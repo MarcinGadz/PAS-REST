9e02c1551d79fa049d69465ff1816bab243712ba
mtosik <michal.tosik@wp.pl>
2022-01-11 22:51:21 +0100

    Active / past tickets in user tests added.

--- src/test/java/service/RestAssuredUserControllerTest.java
+++ src/test/java/service/RestAssuredUserControllerTest.java
@@ -1,14 +1,17 @@
 package service;
 
 import com.google.gson.Gson;
-import com.pas.app.model.Hall;
-import com.pas.app.model.Seat;
-import com.pas.app.model.User;
+import com.pas.app.model.*;
+import com.pas.app.utils.GsonLocalDateTime;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.UUID;
 
 import static io.restassured.RestAssured.*;
@@ -16,6 +19,61 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredUserControllerTest {
     User tmp = new User("Nowo", "Dodany", "nowylogin");
+
+    Seat seat = new Seat(2, 5, Hall.B);
+    Film film = new Film(
+            "Title",
+            "Genre",
+            LocalDateTime.of(LocalDate.of(2023, 7, 16), LocalTime.of(14, 45)),
+            LocalDateTime.of(LocalDate.of(2023, 7, 16), LocalTime.of(16, 15)),
+            BigDecimal.valueOf(100)
+    );
+    Film film2 = new Film(
+            "Title",
+            "Genre",
+            LocalDateTime.of(LocalDate.of(2010, 7, 16), LocalTime.of(14, 45)),
+            LocalDateTime.of(LocalDate.of(2010, 7, 16), LocalTime.of(16, 15)),
+            BigDecimal.valueOf(100)
+    );
+    String seatId;
+    String filmId;
+    Gson gson = GsonLocalDateTime.getGsonSerializer();
+
+    private String postTicket(Film f) {
+        Response tempSeat = given().header("Content-type", "application/json")
+                .and().body(new Gson().toJson(seat)).when().post("/api/seat")
+                .then().statusCode(201).extract().response();
+        Response tempUser = get("/api/user/149bf059-f6a3-4c7b-8bfc-a577a485ac32").then().statusCode(200)
+                .extract().response();
+        Response tempFilm = given().header("Content-type", "application/json")
+                .and().body(gson.toJson(f)).when().post("/api/film")
+                .then().statusCode(201).extract().response();
+
+        Ticket ticket = new Ticket(tempUser.body().as(User.class),
+                tempFilm.body().as(Film.class), tempSeat.body().as(Seat.class));
+//        ticket.setId(UUID.fromString("22b24cd6-6b45-11ec-90d6-0242ac120003"));
+
+        Response response = given().header("Content-type", "application/json")
+                .and().body(gson.toJson(ticket)).when().post("/api/ticket")
+                .then().statusCode(201).extract().response();
+
+        seatId = tempSeat.jsonPath().getString("id");
+        filmId = tempFilm.jsonPath().getString("id");
+
+        return response.jsonPath().getString("id");
+    }
+    private void cleanTicket(String id) {
+        given().header("Content-type", "application/json")
+                .when().delete("/api/ticket/" + id)
+                .then().statusCode(202);
+        given().header("Content-type", "application/json")
+                .when().delete("/api/seat/" + seatId)
+                .then().statusCode(202);
+
+        given().header("Content-type", "application/json")
+                .when().delete("/api/film/" + filmId)
+                .then().statusCode(202);
+    }
 
     @Test
     public void userControllerGetListTest() {
@@ -142,23 +200,28 @@ public class RestAssuredUserControllerTest {
                         "login", equalTo("Testlogin2"));
     }
 
-    //TODO po zrobieniu testów ticketa dokonczyc dwa testy ponizej
     @Test
     public void userControllerGetActiveTest() {
         RestAssured.baseURI = "http://localhost:8081";
 
-//        get("/api/user/a3bf9df2-6b2b-11ec-90d6-0242ac120003/active")
-//                .then().statusCode(200).assertThat()
-//                .body("size()", equalTo(1));
+        get("/api/user/149bf059-f6a3-4c7b-8bfc-a577a485ac32/active")
+                .then().statusCode(200).assertThat().body("size()", equalTo(0));
+        String ticketId = postTicket(film);
+        get("/api/user/149bf059-f6a3-4c7b-8bfc-a577a485ac32/active")
+                .then().statusCode(200).assertThat().body("size()", equalTo(1));
+        cleanTicket(ticketId);
     }
 
     @Test
     public void userControllerGetInactiveTest() {
         RestAssured.baseURI = "http://localhost:8081";
 
-//        get("/api/user/a3bf9df2-6b2b-11ec-90d6-0242ac120003/active")
-//                .then().statusCode(200).assertThat()
-//                .body("size()", equalTo(1));
+        get("/api/user/149bf059-f6a3-4c7b-8bfc-a577a485ac32/past")
+                .then().statusCode(200).assertThat().body("size()", equalTo(0));
+        String ticketId = postTicket(film2);
+        get("/api/user/149bf059-f6a3-4c7b-8bfc-a577a485ac32/past")
+                .then().statusCode(200).assertThat().body("size()", equalTo(1));;
+        cleanTicket(ticketId);
     }
 
     @Test
